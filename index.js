ad32295abda4e4593cf5591f5b3cb268948d6540
Maximilian Alba <albam476@gmail.com>
2022-03-30 19:41:57 -0400

    commit tree 9e31098b2d91be45418528e09e8cc202457815e2

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    const cards = [];
+    for (let i = 0; i < names.length; i++){
+        cards[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    }
+    return cards;
+}
+function countDown(number){
+    while (number >= 0){
+        console.log(number);
+        number --;
+    }
+}
